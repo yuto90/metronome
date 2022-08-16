d0c4da123c8d0f819a29937ffb2371b65023cb09
yuto90 <solidusgako4087@gmail.com>
2022-08-16 22:49:31 +0900

    本番用広告を定数として分離

--- lib/home/home_model.dart
+++ lib/home/home_model.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:metronome/property/home_property.dart';
 import 'rhythm/rhythm_model.dart';
 
@@ -9,21 +10,23 @@ class HomeModel extends ChangeNotifier {
   HomeModel(BuildContext context) {
     rhythmModel = RhythmModel(context);
 
-    // バナー広告をインスタンス化
+    /// バナー広告をインスタンス化
     myBanner = BannerAd(
-      //adUnitId: getTestAdBannerUnitId(),
-      adUnitId: 'ca-app-pub-8474156868822041/4228332466',
-
+      adUnitId: debugMode ? getTestAdBannerUnitId() : getAdBannerUnitId(),
       size: AdSize.banner,
       request: const AdRequest(),
       listener: const BannerAdListener(),
     );
+
     // バナー広告の読み込み
     myBanner.load();
   }
   late RhythmModel rhythmModel;
 
   late BannerAd myBanner;
+
+  // todo リリースビルド時は【false】に切り替える ------------------------------------------
+  bool debugMode = true;
 
   // 設定ボタン押下フラグ
   bool isSettings = false;
@@ -35,17 +38,35 @@ class HomeModel extends ChangeNotifier {
   // プロパティ用のclassをインスタンス化
   HomeProperty homeProperty = HomeProperty();
 
-  // プラットフォーム（iOS / Android）に合わせてデモ用広告IDを返す
+  /// プラットフォーム（iOS / Android）に合わせて本番用広告IDを返す
+  String getAdBannerUnitId() {
+    String bannerUnitId = "";
+
+    // Android のとき
+    if (Platform.isAndroid) {
+      // Androidのバナー広告ID
+      bannerUnitId = dotenv.env['BANNER_UNIT_ID_ANDROID']!;
+
+      // iOSのとき
+    } else if (Platform.isIOS) {
+      // iOSのバナー広告ID
+      bannerUnitId = dotenv.env['BANNER_UNIT_ID_IOS']!;
+    }
+    return bannerUnitId;
+  }
+
+  /// プラットフォーム（iOS / Android）に合わせてデモ用広告IDを返す
   String getTestAdBannerUnitId() {
     String testBannerUnitId = "";
+
+    // Android のとき
     if (Platform.isAndroid) {
-      // Android のとき
-      testBannerUnitId =
-          "ca-app-pub-3940256099942544/6300978111"; // Androidのデモ用バナー広告ID
-    } else if (Platform.isIOS) {
+      // Androidのデモ用バナー広告ID
+      testBannerUnitId = "ca-app-pub-3940256099942544/6300978111";
       // iOSのとき
-      testBannerUnitId =
-          "ca-app-pub-3940256099942544/2934735716"; // iOSのデモ用バナー広告ID
+    } else if (Platform.isIOS) {
+      // iOSのデモ用バナー広告ID
+      testBannerUnitId = "ca-app-pub-3940256099942544/2934735716";
     }
     return testBannerUnitId;
   }
